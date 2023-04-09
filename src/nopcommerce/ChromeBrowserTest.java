3c3a872acb81b22e6690fd2e1938f20e6675ba2c
rpatel942017 <rpatel942017@gmail.com>
2023-04-09 23:01:13 +0100

    update

--- src/nopcommerce/ChromeBrowserTest.java
+++ src/nopcommerce/ChromeBrowserTest.java
@@ -1,2 +1,48 @@
-package nopcommerce;public class ChromeBrowserTest {
+package nopcommerce;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeBrowserTest {
+
+    public static void main(String[] args) {
+
+
+        String baseUrl = "https://demo.nopcommerce.com/login?returnUrl=%2";
+
+        //Launch the Chrome Browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open the URL into Browser
+        driver.get(baseUrl);
+        //maximise the Browser
+        driver.manage().window().maximize();
+        //We give Implicit wait to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+
+        //Get the title of the page
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        //Get the current URL
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        //Get page source
+        System.out.println("Page source : " + driver.getPageSource());
+
+        // Find the email field element
+        WebElement emailField = driver.findElement(By.id("Email"));
+        emailField.sendKeys("prime123@gmail.com");
+
+        // Find the password field element and type the password
+        driver.findElement(By.name("Password")).sendKeys("prime123");
+
+        //Close the browser
+        driver.close();
+
+    }
 }
--- src/nopcommerce/MultiBrowserTest.java
+++ src/nopcommerce/MultiBrowserTest.java
@@ -1,2 +1,42 @@
-package nopcommerce;public class MultiBrowserTest {
+package nopcommerce;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowserTest {
+
+    static String browser = "Edge";
+
+    static String baseUrl = "https://demo.nopcommerce.com/login?returnUrl=%2";
+
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("FireFox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+        //Maximise the Browser
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        System.out.println(driver.getTitle());
+
+        //Find the Email Field Element
+        WebElement emailField = driver.findElement(By.id("Email"));
+        // Type the Email address to email field element
+        emailField.sendKeys("prime123@gmail.com");
+
+
+        driver.findElement(By.name("Password")).sendKeys("Prime123");
+
+    }
 }
